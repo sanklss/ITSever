d1765e057ec3fa08c5fdd7d7c277f909a32dc09b
sanklss <167570025+sanklss@users.noreply.github.com>
2024-04-20 20:34:25 +0300

    222
    222

---
+++ LR7.1/LR7.1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LR7._1
+{
+    internal  class Program
+    {
+        public static  async Task Read()
+        {
+            string path = @"C:/Users/Григорий/Desktop/txt.txt";
+            await Task.Run(() =>
+            {
+                using StreamReader reader = new StreamReader(path);
+                {
+                    string s;
+                    string lines;
+                    while ((lines = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(lines);
+                        Thread.Sleep(300);
+                    }
+                }
+            });
+        }
+    }
+    public class Main1
+    {
+        public static void Main()
+        {
+            Program.Read();
+            Console.ReadLine();
+        }
+    }
+}
